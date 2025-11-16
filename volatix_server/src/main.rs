bb824867cd43e32a5bf967de6e8a9eecb161a516
Juan Milkah <juanmilkah@proton.me>
2025-11-16 09:11:12 +0300

    feat: task queues for incoming requests

--- volatix_server/src/main.rs
+++ volatix_server/src/main.rs
@@ -6,7 +6,7 @@ use std::{
     sync::{
         Arc,
         atomic::{AtomicBool, Ordering},
-        mpsc::{Sender, channel},
+        mpsc::{Receiver, Sender, channel},
     },
     thread::JoinHandle,
     time::Duration,
@@ -26,43 +26,6 @@ use crate::process::process_request;
 const DEFAULT_PORT: u16 = 7878;
 // FLush snapshots to disk in this interval
 const SNAPSHOTS_INTERVAL_TIME: u64 = 60 * 5; // In seconds
-
-fn handle_client(
-    mut stream: TcpStream,
-    storage: Arc<parking_lot::RwLock<LockedStorage>>,
-    message_tx: Arc<Sender<Message>>,
-) {
-    let mut buffer = [0; 1024 * 14];
-
-    loop {
-        match stream.read(&mut buffer) {
-            Ok(0) => break,
-            Ok(n) => match parse_request(&buffer[..n]) {
-                Ok(req) => {
-                    let storage = Arc::clone(&storage);
-                    let response = process_request(&req, storage, Arc::clone(&message_tx));
-
-                    if let Err(e) = stream.write_all(&response) {
-                        let _ =
-                            message_tx.send(Message::Error(format!("Writing to tcp Stream: {e}")));
-                    }
-                }
-                Err(err) => {
-                    let err = format!("Invalid request: {err}");
-                    let _ = message_tx.send(Message::Error(format!("Invalid request: {err}")));
-
-                    if let Err(e) = stream.write_all(err.as_bytes()) {
-                        let _ =
-                            message_tx.send(Message::Error(format!("Writing to tcp Stream: {e}")));
-                    }
-                }
-            },
-            Err(e) => {
-                let _ = message_tx.send(Message::Error(format!("Reading from tcp stream: {e}")));
-            }
-        }
-    }
-}
 
 #[derive(Debug, Parser)]
 struct Cli {
@@ -103,6 +66,151 @@ fn get_log_file() -> anyhow::Result<PathBuf> {
 
     let home = Path::new(&home);
     Ok(home.to_path_buf().join(".volatix.logs"))
+}
+
+enum Response {
+    Data { inner: Vec<u8>, stream: TcpStream },
+    Break,
+}
+
+fn write_to_stream(response_rx: Receiver<Response>, message_tx: Arc<Sender<Message>>) {
+    loop {
+        if let Ok(r) = response_rx.recv() {
+            match r {
+                Response::Data { inner, mut stream } => {
+                    if let Err(e) = stream.write_all(&inner) {
+                        let _ =
+                            message_tx.send(Message::Error(format!("Writing to tcp Stream: {e}")));
+                    }
+                }
+                Response::Break => break,
+            }
+        }
+    }
+}
+
+enum Task {
+    Process { data: Vec<u8>, stream: TcpStream },
+    Break,
+}
+
+fn read_from_stream(
+    mut stream: TcpStream,
+    message_tx: Arc<Sender<Message>>,
+    task_tx: Arc<Sender<Task>>,
+) {
+    let mut buffer = vec![0u8; 4096];
+    loop {
+        match stream.read(&mut buffer) {
+            Ok(0) => break, // Client disconnected
+            Ok(n) => {
+                let data = buffer[..n].to_vec();
+
+                // Clone stream for response
+                match stream.try_clone() {
+                    Ok(stream_clone) => {
+                        let _ = task_tx.send(Task::Process {
+                            data,
+                            stream: stream_clone,
+                        });
+                    }
+                    Err(e) => {
+                        let _ = message_tx.send(Message::Error(e.to_string()));
+                        break;
+                    }
+                }
+            }
+            Err(e) if e.kind() == std::io::ErrorKind::WouldBlock => {
+                std::thread::sleep(Duration::from_millis(1));
+                continue;
+            }
+            Err(e) => {
+                let _ = message_tx.send(Message::Error(e.to_string()));
+                break;
+            }
+        }
+    }
+}
+
+fn task_handler(
+    task_rx: Receiver<Task>,
+    storage: Arc<parking_lot::RwLock<LockedStorage>>,
+    message_tx: Arc<Sender<Message>>,
+    response_tx: Arc<Sender<Response>>,
+) {
+    loop {
+        if let Ok(t) = task_rx.recv() {
+            match t {
+                Task::Process { data, stream } => match parse_request(&data) {
+                    Ok(req) => {
+                        let storage = Arc::clone(&storage);
+                        let response = process_request(&req, storage, Arc::clone(&message_tx));
+
+                        let _ = response_tx.send(Response::Data {
+                            inner: response,
+                            stream,
+                        });
+                    }
+                    Err(err) => {
+                        let err = format!("Invalid request: {err}");
+                        let _ = message_tx.send(Message::Error(format!("Invalid request: {err}")));
+
+                        let _ = response_tx.send(Response::Data {
+                            inner: err.as_bytes().to_vec(),
+                            stream,
+                        });
+                    }
+                },
+                Task::Break => break,
+            }
+        }
+    }
+}
+
+fn snapshots_handler(
+    interval: u64,
+    storage: Arc<RwLock<LockedStorage>>,
+    shutdown: Arc<AtomicBool>,
+    path: &PathBuf,
+) {
+    loop {
+        for _ in 0..(interval) / 5 {
+            if shutdown.load(Ordering::Relaxed) {
+                return;
+            }
+            std::thread::sleep(Duration::from_secs(5));
+        }
+
+        if storage.read().should_flush() && storage.read().save_to_disk(path).is_ok() {
+            storage.write().toggle_dirty_flag();
+        }
+    }
+}
+
+fn client_handler(
+    listener: TcpListener,
+    shutdown: Arc<AtomicBool>,
+    message_tx: Arc<Sender<Message>>,
+    task_tx: Arc<Sender<Task>>,
+) {
+    loop {
+        if shutdown.load(Ordering::Relaxed) {
+            break;
+        }
+        match listener.accept() {
+            Ok((client, _)) => {
+                let message_tx = Arc::clone(&message_tx);
+                let task_tx = Arc::clone(&task_tx);
+                std::thread::spawn(move || read_from_stream(client, message_tx, task_tx));
+            }
+            Err(err) if err.kind() == std::io::ErrorKind::WouldBlock => {
+                std::thread::sleep(Duration::from_millis(1));
+            }
+            Err(_) => {
+                continue;
+            }
+        }
+    }
 }
 
 pub struct Worker {
@@ -159,51 +267,51 @@ fn main() -> anyhow::Result<()> {
         id: "snapshots_handler".into(),
         handle: std::thread::spawn(move || {
             let interval = args.snapshots_interval.unwrap_or(SNAPSHOTS_INTERVAL_TIME);
-            loop {
-                for _ in 0..(interval) / 5 {
-                    if snapshots_shutdown.load(Ordering::Relaxed) {
-                        return;
-                    }
-                    std::thread::sleep(Duration::from_secs(5));
-                }
-
-                if snapshots_storage.read().should_flush()
-                    && snapshots_storage
-                        .read()
-                        .save_to_disk(&snapshots_persistent_path)
-                        .is_ok()
-                {
-                    snapshots_storage.write().toggle_dirty_flag();
-                }
-            }
+            snapshots_handler(
+                interval,
+                snapshots_storage,
+                snapshots_shutdown,
+                &snapshots_persistent_path,
+            );
         }),
     });
 
+    let (task_tx, task_rx) = channel::<Task>();
+    let task_tx = Arc::new(task_tx);
+
     let listener_shutdown = Arc::clone(&shutdown);
-    let listener_storage = Arc::clone(&storage);
     let listener_message_tx = Arc::clone(&message_tx);
+    let listener_task_tx = Arc::clone(&task_tx);
+
     workers.push(Worker {
         id: "client_handler".into(),
         handle: std::thread::spawn(move || {
-            loop {
-                if listener_shutdown.load(Ordering::Relaxed) {
-                    break;
-                }
-                match listener.accept() {
-                    Ok((client, _)) => {
-                        let client_storage = Arc::clone(&listener_storage);
-                        let message_tx = Arc::clone(&listener_message_tx);
-                        handle_client(client, client_storage, message_tx);
-                    }
-                    Err(err) if err.kind() == std::io::ErrorKind::WouldBlock => {
-                        std::thread::sleep(Duration::from_millis(10));
-                    }
-                    Err(_) => {
-                        continue;
-                    }
-                }
-            }
+            client_handler(
+                listener,
+                listener_shutdown,
+                listener_message_tx,
+                listener_task_tx,
+            );
         }),
+    });
+
+    let (response_tx, response_rx) = channel::<Response>();
+    let response_tx = Arc::new(response_tx);
+
+    let handler_storage = Arc::clone(&storage);
+    let handler_message_tx = Arc::clone(&message_tx);
+    let handler_res_tx = Arc::clone(&response_tx);
+    workers.push(Worker {
+        id: "task_handler".into(),
+        handle: std::thread::spawn(move || {
+            task_handler(task_rx, handler_storage, handler_message_tx, handler_res_tx)
+        }),
+    });
+
+    let responder_message_tx = Arc::clone(&message_tx);
+    workers.push(Worker {
+        id: "response_writer".into(),
+        handle: std::thread::spawn(move || write_to_stream(response_rx, responder_message_tx)),
     });
 
     while !shutdown.load(Ordering::Relaxed) {
@@ -211,7 +319,9 @@ fn main() -> anyhow::Result<()> {
     }
 
     // Kill the logs message handler
-    let _res = message_tx.send(Message::Break);
+    let _ = message_tx.send(Message::Break);
+    let _ = response_tx.send(Response::Break);
+    let _ = task_tx.send(Task::Break);
 
     for worker in workers {
         println!("Cleaning up {}", worker.id);
